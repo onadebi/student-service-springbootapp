728547fd654ac7b3e29613acec3c63738f4618c1
Onadebi <onadebi@onadebi.com>
2023-12-23 22:42:02 -0500

    Addition of StudentConfig class toseed database on app startup run

--- src/main/java/com/onadebi/demo/student/Student.java
+++ src/main/java/com/onadebi/demo/student/Student.java
@@ -2,8 +2,27 @@ package com.onadebi.demo.student;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
+
+
     private String name;
     private String email;
     private LocalDate dob;
--- src/main/java/com/onadebi/demo/student/StudentConfig.java
+++ src/main/java/com/onadebi/demo/student/StudentConfig.java
@@ -1,2 +1,25 @@
-package com.onadebi.demo.student;public class StudentConfig {
+package com.onadebi.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            
+            List<Student> allStudents = List.of(
+                    new Student("Onaefe Edebi", "onaefe@onadebi.com", LocalDate.of(2023, Calendar.SEPTEMBER, 23),
+                            150),
+                    new Student("John Doe", "j.doe@onadebi.com", LocalDate.of(2023, Calendar.SEPTEMBER, 23), 250));
+            
+            studentRepository.saveAll(allStudents);
+        };
+    }
 }
